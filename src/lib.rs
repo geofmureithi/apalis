64ba1c43af7d7836a341410853248a4f806e7b0e
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2020-04-23 11:27:09 +0300

    Add lib and exports

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,13 @@ mod error;
 mod message;
 mod producer;
 
+// Am I doing this right?
+pub use actor::QueueActor;
+pub use actor::*;
+pub use consumer::Consumer;
+pub use producer::Producer;
+pub use error::TaskError;
+
 #[cfg(test)]
 mod tests {
     #[test]
