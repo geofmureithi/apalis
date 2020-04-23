2242f4377b46fd3a4ea064020f97b066ebd78dfd
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2020-04-23 11:21:20 +0300

    Add Error Handling

---
+++ src/error.rs
@@ -0,0 +1,28 @@
+use redis::RedisError;
+
+#[derive(Debug)]
+pub enum TaskError {
+    Redis(RedisError),
+    AlreadyInQueue(String),
+    InThePast,
+    Execution(failure::Error),
+}
+impl std::fmt::Display for TaskError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "TaskError!")
+    }
+}
+
+impl From<RedisError> for TaskError {
+    fn from(err: RedisError) -> Self {
+        TaskError::Redis(err)
+    }
+}
+
+impl From<failure::Error> for TaskError {
+    fn from(err: failure::Error) -> Self {
+        TaskError::Execution(err)
+    }
+}
+
+impl std::error::Error for TaskError {}
