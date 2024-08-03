15904348411b4e15d3a7fd1f5a411db30e231ea5
geofmureithi <mureithinjuguna@gmail.com>
2024-08-03 15:27:21 +0300

    fix: sql tests

--- packages/apalis-sql/src/lib.rs
+++ packages/apalis-sql/src/lib.rs
@@ -166,7 +166,7 @@ macro_rules! sql_storage_tests {
             let (job_id, res) = storage.execute_next().await;
             assert_eq!(
                 res,
-                Err("AbortError: Invalid character. Job killed".to_owned())
+                Err("AbortError: Invalid character.".to_owned())
             );
             apalis_core::sleep(Duration::from_secs(1)).await;
             let job = storage.fetch_by_id(&job_id).await.unwrap().unwrap();
@@ -175,7 +175,7 @@ macro_rules! sql_storage_tests {
             assert!(ctx.done_at().is_some());
             assert_eq!(
                 ctx.last_error().clone().unwrap(),
-                "{\"Err\":\"AbortError: Invalid character. Job killed\"}"
+                "{\"Err\":\"AbortError: Invalid character.\"}"
             );
         }
 
