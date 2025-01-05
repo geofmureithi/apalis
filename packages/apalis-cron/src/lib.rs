535e50c2d17ae40b2abb07414007d2b157f8dbc4
Geoffrey Mureithi <95377562+geofmureithi@users.noreply.github.com>
2025-01-05 08:03:25 +0300

    chore: improve several examples (#489)
    * chore: improve several examples
    
    * lint: cargo fmt

--- packages/apalis-cron/src/lib.rs
+++ packages/apalis-cron/src/lib.rs
@@ -9,7 +9,7 @@
 
 //! # apalis-cron
 //! A simple yet extensible library for cron-like job scheduling for rust.
-//! Since `apalis-cron` is build on top of `apalis` which supports tower middlerware, you should be able to easily
+//! Since `apalis-cron` is build on top of `apalis` which supports tower middleware, you should be able to easily
 //! add middleware such as tracing, retries, load-shed, concurrency etc.
 //!
 //! ## Example
@@ -23,11 +23,11 @@
 //! #[derive(Default, Debug, Clone)]
 //! struct Reminder(DateTime<Utc>);
 //!
-//! # impl From<DateTime<Utc>> for Reminder {
-//! #    fn from(t: DateTime<Utc>) -> Self {
-//! #        Reminder(t)
-//! #    }
-//! # }
+//! impl From<DateTime<Utc>> for Reminder {
+//!    fn from(t: DateTime<Utc>) -> Self {
+//!        Reminder(t)
+//!    }
+//! }
 //!
 //! async fn handle_tick(job: Reminder, data: Data<usize>) {
 //!     // Do something with the current tick
@@ -46,6 +46,35 @@
 //!     worker.run().await;
 //! }
 //! ```
+//! ## Persisting cron jobs
+//!
+//! Sometimes we may want to persist cron jobs for several reasons:
+//!
+//! - Distribute cronjobs between multiple servers
+//! - Store the results of the cronjob
+//! - Prevent task skipping in the case of a restart
+//!
+//! ```rust,no_run
+//! #[tokio::main]
+//! async fn main() {
+//!     let schedule = Schedule::from_str("@daily").unwrap();
+//!     let cron_stream = CronStream::new(schedule);
+//!
+//!     // Lets create a storage for our cron jobs
+//!     let pool = SqlitePool::connect("sqlite::memory:").await.unwrap();
+//!     SqliteStorage::setup(&pool)
+//!         .await
+//!         .expect("unable to run migrations for sqlite");
+//!     let sqlite = SqliteStorage::new(pool);
+//!
+//!     let backend = cron_stream.pipe_to_storage(sqlite);
+//!
+//!     let worker = WorkerBuilder::new("morning-cereal")
+//!         .backend(backend)
+//!         .build_fn(handle_tick);
+//!
+//!     worker.run().await;
+//! }
 //! ```
 
 use apalis_core::backend::Backend;
