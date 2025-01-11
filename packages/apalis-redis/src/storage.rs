c6eec5906877e4e52d6f1814234d218040373e78
Zak Stucke <zakstucke@hotmail.co.uk>
2025-01-11 21:31:07 +0000

    fmt

--- packages/apalis-redis/src/storage.rs
+++ packages/apalis-redis/src/storage.rs
@@ -632,7 +632,9 @@ where
             }
             Err(e) => {
                 warn!("An error occurred during streaming jobs: {e}");
-                if matches!(e.kind(), ErrorKind::ResponseError) && e.to_string().contains("consumer not registered script") {
+                if matches!(e.kind(), ErrorKind::ResponseError)
+                    && e.to_string().contains("consumer not registered script")
+                {
                     self.keep_alive(worker_id).await?;
                 }
                 Err(e)
